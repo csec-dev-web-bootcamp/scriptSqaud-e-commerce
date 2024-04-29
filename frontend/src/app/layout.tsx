200123f3206f21267ec5a51932650b5f06dba66d
Natnael <ngirma700@gmail.com>
2024-04-29 14:20:54 +0300

    Initialize frontend directory structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ 
+  subsets: ["latin"],
+  variable: "--font-sans", 
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+          " bg-background min-h-screen font-sans antialiased",
+          inter.variable
+        )}>
+        {children}
+        </body>
     </html>
   );
 }
