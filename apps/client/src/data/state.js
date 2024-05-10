49075fe0e5c3b634b3efdb61bfaa4fdc241f3cb0
Naol Ketema <naolketema55@gmail.com>
2024-05-10 18:36:19 +0300

    something updated

--- apps/client/src/data/state.js
+++ apps/client/src/data/state.js
@@ -1,21 +1,60 @@
 import { create } from "zustand";
+
 export const useCart = create((set) => ({
   cartProducts: [],
 
   addToCart: (product) =>
     set((state) => {
       const currentState = JSON.parse(JSON.stringify(state));
-      currentState.cartProducts.push(product);
+      currentState.cartProducts.push({
+        ...product,
+        totalPrice: product.price,
+        amount: 1,
+      });
 
       return currentState;
     }),
 
-  removeProductFromCart: (id) =>
+  removeFromCart: (id) =>
     set((state) => {
       const currentState = JSON.parse(JSON.stringify(state));
 
       currentState.cartProducts = currentState.cartProducts.filter(
         (product) => product.id !== id
+      );
+
+      return currentState;
+    }),
+
+  addProductAmount: (id) =>
+    set((state) => {
+      const currentState = JSON.parse(JSON.stringify(state));
+
+      currentState.cartProducts = currentState.cartProducts.map((product) =>
+        product.id === id
+          ? {
+              ...product,
+              amount: product.amount + 1,
+              totalPrice: product.price * (product.amount + 1),
+            }
+          : product
+      );
+
+      return currentState;
+    }),
+
+  minusProductAmount: (id) =>
+    set((state) => {
+      const currentState = JSON.parse(JSON.stringify(state));
+
+      currentState.cartProducts = currentState.cartProducts.map((product) =>
+        product.id === id && product.amount > 1
+          ? {
+              ...product,
+              amount: product.amount - 1,
+              totalPrice: product.price * (product.amount - 1),
+            }
+          : product
       );
 
       return currentState;
