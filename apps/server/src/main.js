56fb76d5a99b4c30c2ab4dcb0f7faa6de24fd254
Naol Ketema <naolketema55@gmail.com>
2024-05-14 23:41:04 +0300

    revert  5dfb8bd65beccef7454fa29a90383dcf2cc6db61

--- apps/server/src/main.js
+++ apps/server/src/main.js
@@ -2,9 +2,6 @@ import "dotenv/config";
 import express from "express";
 import { httpExceptionHandler } from "./middlewares/http-exception-handler";
 import postsController from "./posts/posts.controller";
-import authController from './auth/auth.controller';
-import productsController from './products/products.controller';
-
 import cors from "cors";
 import { corsOptions } from "./constants/cors-options";
 
@@ -20,8 +17,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/posts", postsController);
-app.use('/products', productsController);
-app.use('/auth', authController);
 
 app.all("*", (req, res) => {
   return res.status(404).json({ error: "Not Found" });
