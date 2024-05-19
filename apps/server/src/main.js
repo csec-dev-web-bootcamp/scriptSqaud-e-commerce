81621a3f9e60dd491b23d76ad456db68afab08a6
Webi Muleta <133377877+Dirac1235@users.noreply.github.com>
2024-05-20 00:29:31 +0300

    correction changes

--- apps/server/src/main.js
+++ apps/server/src/main.js
@@ -4,6 +4,7 @@ import { httpExceptionHandler } from "./middlewares/http-exception-handler";
 import postsController from "./posts/posts.controller";
 import cors from "cors";
 import { corsOptions } from "./constants/cors-options";
+import authController from "./auth/auth.controller";
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/posts", postsController);
-
+app.use("/auth", authController)
 app.all("*", (req, res) => {
   return res.status(404).json({ error: "Not Found" });
 });
