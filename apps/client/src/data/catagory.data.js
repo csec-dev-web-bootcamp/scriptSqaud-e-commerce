52f64490b256c2644cb97fbec7901cdf44d7d49c
Webi Muleta <133377877+Dirac1235@users.noreply.github.com>
2024-05-28 21:06:44 +0300

    made the frontend to work and adjusted the categories api

--- apps/client/src/data/catagory.data.js
+++ apps/client/src/data/catagory.data.js
@@ -5,28 +5,28 @@ import fetcher from './fetcher';
 import revalidate from './revalidate';
 
 export async function createCategory(data,) {
-    const catagory = fetcher.post('/category', data);
+    const catagory = fetcher.post('/categories', data);
     revalidate({ tags: ['CATAGORIES'] });
     return catagory;
 }
 
 export async function getManyCategories() {
-    const res = await fetcher.get('/category');
+    const res = await fetcher.get('/categories');
     return res.data;
 }
 
 export async function getOneCategory(id,) {
-    const res = await fetcher.get(`/category/${id}`);
+    const res = await fetcher.get(`/categories/${id}`);
     return res.data;
 }
 
 export async function updateCategory(id, data) {
-    const res = await fetcher.put(`/category/${id}`, data);
+    const res = await fetcher.put(`/categories/${id}`, data);
     return res.data;
 }
 
 export async function deleteCatagory(id) {
-    const res = await fetcher.get(`/category/${id}`);
+    const res = await fetcher.get(`/categories/${id}`);
     return res.data;
 }
 
